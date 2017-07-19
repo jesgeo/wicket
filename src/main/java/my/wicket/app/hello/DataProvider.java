4ef775a2ea972a278029922b404555162bb1ad66
Jestin George <jesgeo@users.noreply.github.com>
2017-07-19 14:08:36 +0100

    adding sorting comparator

--- src/main/java/my/wicket/app/hello/DataProvider.java
+++ src/main/java/my/wicket/app/hello/DataProvider.java
@@ -1,26 +1,42 @@
 package my.wicket.app.hello;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Objects;
 
+import org.apache.wicket.core.util.lang.PropertyResolver;
 import org.apache.wicket.extensions.markup.html.repeater.data.sort.ISortState;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.ISortableDataProvider;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.filter.IFilterStateLocator;
 import org.apache.wicket.extensions.markup.html.repeater.util.SingleSortState;
+import org.apache.wicket.extensions.markup.html.repeater.util.SortableDataProvider;
+import org.apache.wicket.model.AbstractReadOnlyModel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
+import org.apache.wicket.model.PropertyModel;
 
-
-public class DataProvider implements ISortableDataProvider<Customer, String>, IFilterStateLocator<Customer> {
+public class DataProvider extends SortableDataProvider<Customer, String> implements IFilterStateLocator<Customer> {
 
 	private CustomerService service = CustomerService.getInstance();
-	
-	
+
+	private SortableDataProviderComparator comparator = new SortableDataProviderComparator();
+
 	private Customer searchEntity = new Customer();
-	
-	
+
+	public DataProvider() {
+		// this.setSort(param);setSort("name.first", true);
+	}
+
 	@Override
 	public Iterator<? extends Customer> iterator(long first, long count) {
-		return service.findAll(searchEntity.getFirstName(), (int)first, (int)count).iterator();
+		List<Customer> list = service.findAll(searchEntity.getFirstName(), (int) first, (int) count);
+		Collections.sort(list, comparator);
+		return list.iterator();
 	}
 
 	@Override
@@ -29,17 +45,14 @@ public class DataProvider implements ISortableDataProvider<Customer, String>, IF
 		return service.count();
 	}
 
-
-	
 	@Override
-	public IModel<Customer> model(Customer object) {
-		return Model.of(object);
-	}
-
-	//need to implement this for sorting
-	@Override
-	public ISortState<String> getSortState() {
-		return new SingleSortState<String>();
+	public IModel<Customer> model(final Customer object) {
+		return new AbstractReadOnlyModel<Customer>() {
+			@Override
+			public Customer getObject() {
+				return object;
+			}
+		};
 	}
 
 	@Override
@@ -51,6 +64,33 @@ public class DataProvider implements ISortableDataProvider<Customer, String>, IF
 	public void setFilterState(Customer state) {
 		// TODO Auto-generated method stub
 		searchEntity = state;
+
+	}
+
+	class SortableDataProviderComparator implements Comparator<Customer>, Serializable {
+		public int compare(final Customer o1, final Customer o2) {
+
+			if (getSort() == null || getSort().getProperty() == null) {
+				return 0;
+			}
+
+			PropertyModel<Comparable> model1 = new PropertyModel<Comparable>(o1, getSort().getProperty());
+			PropertyModel<Comparable> model2 = new PropertyModel<Comparable>(o2, getSort().getProperty());
+
+			// String property = getSort().getProperty();
+			//
+			// Object value1 = PropertyResolver.getValue(property, o1);
+			// Object value2 = PropertyResolver.getValue(property, o2);
+
+			int result = model1.getObject().compareTo(model2.getObject());
+
+			if (!getSort().isAscending()) {
+				result = -result;
+			}
+
+			return result;
+		}
+
 	}
 
 }
